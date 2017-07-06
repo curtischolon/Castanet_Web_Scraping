fee13680e0986d374fa8f6ef85e520d6bd89acbd
Curtis Cholon <curtis.cholon@gmail.com>
2017-07-06 08:49:41 -0700

    Removed redundant commands

--- castanet_smoker_scrapper.py
+++ castanet_smoker_scrapper.py
@@ -1,13 +1,11 @@
 import requests
-import bs4
 from bs4 import BeautifulSoup
 
 import pandas as pd
 import time
 
 # Search for smoker on Castanet.net
-URL = "https://classifieds.castanet.net/search/?search=smoker&\
-    id_cat=254&pfrom=&pto=&datetype=days&user=&datenr=&submit="
+URL = "https://classifieds.castanet.net/cat/house-home/garden_yard_patio/bbqs_smokers_heaters/"
 
 # requesting the page above
 page = requests.get(URL)
@@ -18,7 +16,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 # print(soup.prettify())
 
 def extract_listing_title_from_result(soup):
-
+    """pulls the ad title for each listing on the current page"""
     ads = []
     for prod_container in soup.find_all(name='a', class_='prod_container'):
         for div in prod_container.find(name='h2'):
@@ -27,7 +25,33 @@ def extract_listing_title_from_result(soup):
 
     return ads
 
+def extract_listing_price_from_result(soup):
+    """pulls the listing price for each listing on the current page"""
+    prices = []
+    for description in soup.find_all(name = 'div', class_ = 'descr'):
+            for price in description.find(name = 'div', class_ = 'price'):
+                print(price)
+                prices.append(price)
+
+    return prices
+
+def extract_listing_location_from_result(soup):
+    """pulls the listing location for each listing on the current page"""
+    location = []
+    for div in soup.find_all(name = 'div', class_ = 'pdate'):
+        for city in div.find(name = 'span'):
+            print(city)
+            location.append(city)
+
+    return location
+
 
 ads = extract_listing_title_from_result(soup)
 
+prices = extract_listing_price_from_result(soup)
+
+locations = extract_listing_location_from_result(soup)
+
 print(ads)
+print(prices)
+print(locations)
