831575e32361faaa094bf44c5cb66efd1b897202
Curtis Cholon <curtis.cholon@gmail.com>
2017-07-07 16:10:46 -0700

    Added file download functionality

--- castanet_smoker_scrapper.py
+++ castanet_smoker_scrapper.py
@@ -1,22 +1,14 @@
+#! python3
+
 import requests
 import re
 from bs4 import BeautifulSoup
 import os
+import time
 
 import pandas as pd
 import time
 
-# Search for smoker on Castanet.net
-URL = "https://classifieds.castanet.net/cat/house-home/garden_yard_patio/bbqs_smokers_heaters/"
-
-
-# requesting the page above
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.text, 'html.parser')
-
-
-# input(soup.prettify())
 
 def find_number_of_ads():
     """returns the number of ads to parse"""
@@ -31,15 +23,17 @@ def find_number_of_ads():
     number_regex = re.compile(r'(\d)*')
     number_of_ads = number_regex.search(number_of_ads).group()
 
+    print("Number of ads: ", number_of_ads)
+
     return int(number_of_ads)
 
 
 def download_image(urls):
     """downloads listing images and saves locally"""
+    image_paths = []
+
     base_url = "https://classifieds.castanet.net"
     image_directory = os.path.join('C:\\', 'users', 'ccholon', 'my documents', 'castanet images')
-
-    image_paths = []
 
     for url in urls:
         listing_url = base_url + url
@@ -66,68 +60,92 @@ def download_image(urls):
     return image_paths
 
 
-def extract_listing_title_from_result(soup):
+def extract_listing_title_from_result(soup, ads, ad_count):
     """pulls the ad title for each listing on the current page"""
-    ads = []
-    add_count = 0
     for prod_container in soup.find_all(name='a', class_='prod_container'):
         for div in prod_container.find(name='h2'):
             ads.append(div)
-            add_count += 1
+            ad_count += 1
 
-    return ads, add_count
+    print(ads)
+    print(ad_count)
+    return ads, ad_count
 
 
-def extract_listing_price_from_result(soup):
+def extract_listing_price_from_result(soup, prices):
     """pulls the listing price for each listing on the current page"""
-    prices = []
     for description in soup.find_all(name='div', class_='descr'):
         for price in description.find_all(name='div', class_='price'):
             prices.append(price.get_text())
-
+    print(prices)
     return prices
 
 
-def extract_listing_location_from_result(soup):
+def extract_listing_location_from_result(soup, location):
     """pulls the listing location for each listing on the current page"""
-    location = []
     for div in soup.find_all(name='div', class_='pdate'):
         for city in div.find(name='span'):
             location.append(city)
-
+    print(locations)
     return location
 
 
-def extract_url_from_result(soup):
+def extract_url_from_result(soup, url, listing_id):
     """pulls the url for each listing on the current page"""
-    url = []
-    listing_id = []
     for link in soup.find_all(name='a', class_='prod_container'):
         url.append(link.get('href'))
         link_text = link.get('href')
 
         # From URL - extract listing ID
-        id_regex = re.compile(r'\d{7}')
-        id_ = id_regex.search(link_text).group()
-        listing_id.append(id_)
-
+        try:
+            id_regex = re.compile(r'\d{7}')
+            id_ = id_regex.search(link_text).group()
+            listing_id.append(id_)
+        except:
+            listing_id.append('Not found')
+    print(url)
+    print(listing_id)
     return url, listing_id
 
 
-
 # --MAIN--
+# base URL - Castanet BBQ, Smokers, Heaters
+base_url = "https://classifieds.castanet.net/cat/house-home/garden_yard_patio/bbqs_smokers_heaters/?p="
 
-ads, add_count = extract_listing_title_from_result(soup)
-
-prices = extract_listing_price_from_result(soup)
-
-locations = extract_listing_location_from_result(soup)
-
-urls, ids = extract_url_from_result(soup)
-
+# first step - determine the total number of ads
 number_of_ads = find_number_of_ads()
 
-images = download_image(urls)
+# based on the total number of add, cycle through each page
+ad_count = 0
+page = 1
+ads = []
+prices = []
+locations = []
+urls = []
+ids = []
+while ad_count < number_of_ads:
+    # create custom search url based on number of ads
+    url = base_url + str(page)
+
+    # requesting the page above
+    web_page = requests.get(url)
+    time.sleep(1)
+
+    soup = BeautifulSoup(web_page.text, 'html.parser')
+
+    # extract ad details
+    ads, ad_count = extract_listing_title_from_result(soup, ads, ad_count)
+
+    prices = extract_listing_price_from_result(soup, prices)
+
+    locations = extract_listing_location_from_result(soup, locations)
+
+    urls, ids = extract_url_from_result(soup, urls, ids)
+
+    page += 1
+
+# TODO - add feature that compares the listing details to what has already been downloaded - skip duplicates
+image_list = download_image(urls)
 
 print(ads)
 print(prices)
@@ -135,5 +153,5 @@ print(locations)
 print(urls)
 print(ids)
 print(number_of_ads)
-print(add_count)
-print(images)
+print(ad_count)
+print(image_list)
